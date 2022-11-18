8b25478b3755b223e7055c4a3c7feff5456298a2
moerp <matthias.hottgenroth@icloud.com>
2022-11-18 10:00:45 +0100

    add comments

--- crypto_prediction/main.py
+++ crypto_prediction/main.py
@@ -15,16 +15,22 @@ def get_dataframe(url, starttime, endtime, symbol='BTCUSDT', interval='1h', limi
 def get_historical_data():
     url = "https://api.binance.com/api/v3/klines"
     hist_df = pd.DataFrame()
+    # get historical BTC data from past year
     for i in range(12, -1, -1):
         starttime = str(int((dt.datetime.today() - relativedelta(months=i+1)).timestamp() * 1000))
         endtime = str(int((dt.datetime.today() - relativedelta(months=i)).timestamp() * 1000))
         hist_df = pd.concat([hist_df, pd.DataFrame(get_dataframe(url, starttime, endtime))])
-
+    # drop everything but opening prices
     hist_np = hist_df.iloc[:, 1].to_numpy().astype(float)
-    #hist_np = (hist_np - np.mean(hist_np)) / np.std(hist_np)
-    plt.plot(range(len(hist_np)), hist_np)
-    plt.show()
+    # standardize data
+    hist_np = (hist_np - np.mean(hist_np)) / np.std(hist_np)
+    #plt.plot(range(len(hist_np)), hist_np)
+    #plt.show()
+    return hist_np
 
+
+def main():
+    hist_np = get_historical_data()
 
 if __name__ == '__main__':
-    get_historical_data()
+    main()
